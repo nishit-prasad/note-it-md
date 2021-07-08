e8c07f91996e15159bf4fef6f25485d916443e80
Nishit Prasad <64563418+nishit-prasad@users.noreply.github.com>
2021-07-09 01:26:32 +0530

    10: Added header and footer components
    Signed-off-by: Nishit Prasad <64563418+nishit-prasad@users.noreply.github.com>

---
+++ src/components/Footer/SocialMediaLinks.js
@@ -0,0 +1,22 @@
+import { ButtonGroup, IconButton } from '@chakra-ui/react';
+import * as React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+export const SocialMediaLinks = props => (
+  <ButtonGroup variant="ghost" color="gray.600" {...props}>
+    <IconButton
+      color="teal"
+      as="a"
+      href="https://www.linkedin.com/in/nishitprasad/"
+      aria-label="LinkedIn"
+      icon={<FaLinkedin fontSize="24px" />}
+    />
+    <IconButton
+      color="teal"
+      as="a"
+      href="#"
+      aria-label="GitHub"
+      icon={<FaGithub fontSize="24px" />}
+    />
+  </ButtonGroup>
+);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
-import { Flex, Heading, Box, Spacer, Button } from '@chakra-ui/react';
+import { Icon, Flex, Heading, Box, Spacer, Button } from '@chakra-ui/react';
+import { HiOutlineBookmarkAlt } from 'react-icons/hi';
 
 const Header = () => {
   return (
-    <Flex boxShadow={'lg'} p={4} rounder={'md'} my={2}>
-      <Box p="2">
-        <Heading size="md">Notes App</Heading>
-      </Box>
+    <Flex
+      borderBottom="2px"
+      borderColor="gray.200"
+      p={4}
+      rounder="md"
+      marginTop={2}
+    >
+      <Flex p="2" alignItems={'center'}>
+        <Icon color="teal" as={HiOutlineBookmarkAlt} w={8} h={8} />
+        <Heading size="md" color="gray.600">
+          Notes App
+        </Heading>
+      </Flex>
       <Spacer />
       <Box>
         <Button colorScheme="teal" mr="4">
           Sign Up
         </Button>
-        <Button colorScheme="teal">Log in</Button>
+        <Button colorScheme="teal">Sign in</Button>
       </Box>
     </Flex>
   );
