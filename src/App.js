e75a58b6ebdb017f3221616e577bbbf049b59de5
Nishit Prasad <prasad.nishit0@gmail.com>
2021-07-10 02:35:51 +0530

    [UI][12]: Sign-In Form containing email, pwd & buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { Flex, Center } from '@chakra-ui/react';
+import {
+  Flex,
+  Center,
+  useDisclosure,
+  ScaleFade,
+  Heading,
+} from '@chakra-ui/react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import AuthCard from './components/Authentication/AuthCard';
 
 const App = () => {
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <Flex w="full" h="100vh" direction="column" justifyContent="space-between">
-      <Header />
-      <Center bg="gray.100" h="100%" color="gray.600" fontSize="4xl">
-        Welcome to Notes App!
+      <Header click={onToggle} />
+      <Center bg="gray.100" h="100vh" color="gray.600" fontSize="4xl">
+        {isOpen ? (
+          <AuthCard isOpen={isOpen} />
+        ) : (
+          <ScaleFade initialScale={0.8} in={!isOpen}>
+            <Heading userSelect="none" fontSize={'4xl'}>
+              Welcome to Notes App!
+            </Heading>
+          </ScaleFade>
+        )}
       </Center>
       <Footer />
     </Flex>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { theme, ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import {
+  theme,
+  ChakraProvider,
+  ColorModeScript,
+  ThemeProvider,
+} from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,10 +11,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <ThemeProvider theme={theme}>
+      <ColorModeScript />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
 );
